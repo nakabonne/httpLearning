9bfed4b11c7d369053af4dd0283ea142b1bf6d44
ryonakao <rodriguez.nak@gmail.com>
2017-10-10 14:50:09 +0900

    Add delete

--- http1.0/get/simple_get.go
+++ http1.0/get/simple_get.go
@@ -12,7 +12,10 @@ func simpleGet() {
 		"query": {"hello world"},
 	}
 
-	resp, _ := http.Get("http://localhost:8080" + "?" + values.Encode())
+	resp, err := http.Get("http://localhost:8080" + "?" + values.Encode())
+	if err != nil {
+		panic(err)
+	}
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
 	log.Println(string(body))
