968dfd8bb41b7c5cc5229fe9490ccc99285414e1
ryonakao <rodriguez.nak@gmail.com>
2017-10-10 14:42:02 +0900

    Add proxy.go

--- http1.0/get/main.go
+++ http1.0/get/main.go
@@ -9,6 +9,8 @@ func main() {
 		simpleGet()
 	case 2:
 		cookie()
+	case 3:
+		proxy()
 	default:
 		fmt.Println("数字が違います")
 		main()
@@ -17,7 +19,7 @@ func main() {
 
 func standardIn() int {
 	fmt.Println("メソッドを選択して下さい")
-	fmt.Println("1 単純なGetメソッド\n2 クッキー送信")
+	fmt.Println("1 単純なGetメソッド\n2 クッキー送信\n3 プロキシ送信")
 	var i int
 	fmt.Scan(&i)
 	return i
